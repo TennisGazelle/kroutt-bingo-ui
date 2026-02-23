03509a0da53011692a39e63f303e7c15010caddb
Daniel Lopez <daniel@simplerose.com>
2026-02-23 01:04:06 -0800

    first pass with minimal board, human made

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Box, Button, Card, CardContent, Stack, Typography } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#8e24aa',
+    },
+    secondary: {
+      main: '#3d5afe',
+    },
+  },
+});
+
+interface BingoButtonProps {
+  number?: number;
+  text?: string;
+}
+
+const BingoButton = ({ number = 0, text }: BingoButtonProps) => {
+  const [isClicked, setIsClicked] = useState(false);
+  const variant = text ? "contained" : isClicked ? "contained" : "outlined";
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+    console.log(number, "clicked:", isClicked);
+  };
+
+  return (
+    <Button variant={variant} onClick={handleClick} disabled={!!text}>
+      {text || number}
+    </Button>
+  );
+};
+
+const BingoBoard = () => {
+  const rowHeaders = ["B", "I", "N", "G", "O"];
+  return (
+    <Card variant='outlined' sx={{ height: '100%', display: 'inline-block', justifyContent: 'center', alignItems: 'center', mt: 10 }}>
+      <CardContent>
+        <Stack direction="column" spacing={2.5} key="board">
+          {Array.from({ length: 5 }).map((_, rowIndex) => (
+            <Stack direction="row" spacing={2} key={rowIndex}>
+              <Typography variant="h4" key={rowIndex} sx={{ fontWeight: 'bold', textAlign: 'center', width: '100%' }}>
+                {rowHeaders[rowIndex]}
+              </Typography>
+              {Array.from({ length: 15 }).map((_, cellIndex) => (
+                <BingoButton number={cellIndex + (rowIndex * 15) + 1} />
+              ))}
+            </Stack>
+          ))}
+        </Stack>
+      </CardContent>
+    </Card>
+    // <table className="bingo-board">
+    //   <tbody>
+    //     {Array.from({ length: 5 }).map((_, rowIndex) => (
+    //       <tr key={rowIndex}>
+    //         {Array.from({ length: 15 }).map((_, cellIndex) => (
+    //           <td key={cellIndex + (rowIndex * 15) + 1}>
+    //             <BingoButton number={cellIndex + (rowIndex * 15) + 1} />
+    //           </td>
+    //         ))}
+    //       </tr>
+    //     ))}
+    //   </tbody>
+    // </table>
+  )
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +87,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <BingoBoard />
     </div>
   );
 }
